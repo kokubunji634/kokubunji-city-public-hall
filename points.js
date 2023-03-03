87efc3f374d092610184050026c3daafd4e5ccf9
yukik20 <64530450+yukik20@users.noreply.github.com>
2023-03-03 22:43:03 +0900

    Add files via upload

---
+++ points.js
@@ -0,0 +1,13 @@
+//
+//  このファイルのpointsに、例に倣って表示したい情報を追加していくこと
+//  緯度・経度情報は、GoogleMap上で右クリックすれば取得できる
+//
+var points = [
+//    {
+//        name:"ひかりプラザ", latitude:[35.70284435233955], longitude:[139.44459287320157],
+//        desc:"光町の由来となった新幹線ひかり号！運転手さんごっこもできます。見学は午前9時から午後5時まで。",
+//        link:"https://www.instagram.com/p/Cjh-jsYJSo1/",img:"001_hikari.jpg",
+//        link2:"https://www.city.kokubunji.tokyo.jp/shisetsu/shikanren/shiyakusyo/1003818.html",
+//        value:"blue"
+//    }
+];
